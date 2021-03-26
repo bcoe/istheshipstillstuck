7e22d203a75a52821f13a6444760a44c91b0c8fb
bcoe <bencoe@google.com>
2021-03-25 23:05:50 -0700

    docs: add some todo comments

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// TODO(bcoe): find way to use more bleeding edge Node.js features:
 import fetch from  'node-fetch';
 const resp = await fetch('https://istheshipstillstuck.com/', {
   method: 'get'
@@ -9,6 +10,7 @@ if (resp.status !== 200) {
 } else {
   // Scrape https://istheshipstillstuck.com/
   const content = await resp.text()
+  // TODO(@bcoe): this regex is an abomination, make it better:
   const match = content.match(/6913909783548431618[^>]+\>(?<answer>[^<]+)/)
   console.info(match.groups.answer)
 }
